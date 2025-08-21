103f714c00bc7198c324e71d0c0daf0edf639575
Bobby-21 <frbobby21@gmail.com>
2025-08-21 18:16:36 -0400

    Install platformio with pip and not curl

--- scripts/FlashArduino.sh
+++ scripts/FlashArduino.sh
@@ -6,17 +6,16 @@ PROJECT_DIR="$HOME/ETL_Testing_GUI/drivers/Arduino/"
 
 cd "$PROJECT_DIR"
 
-# Check and install curl if needed
-if ! command -v curl >/dev/null 2>&1; then
-    echo "Installing curl..."
-    sudo apt install -y curl
+# Check and install pip if needed
+if ! command -v pip >/dev/null 2>&1; then
+    echo "Installing pip..."
+    sudo apt install -y pip
 fi
 
 # Check and install PlatformIO if needed
 if ! command -v pio >/dev/null 2>&1; then
     echo "Installing PlatformIO..."
-    curl -fsSL get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
-    python3 get-platformio.py
+    python3 -m pip install -U platformio
     export PATH=$PATH:~/.local/bin
 fi
 

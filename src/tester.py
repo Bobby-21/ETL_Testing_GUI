fc71dff38117413d06ff0ca59f4811fad27290d7
Bobby-21 <frbobby21@gmail.com>
2025-08-22 22:16:54 -0400

    Bugfix

--- src/tester.py
+++ src/tester.py
@@ -6,10 +6,10 @@ def run_uv_script(script_path, test_name, kcu_ip, moduleid, qinj, charges):
     if test_name == "tamalero":
         cmd = ["uv", "run", "python3", script_path, "--kcu", kcu_ip, "--power_up"]
         subprocess.run(cmd)
-    elif test_name == "module" and qinj == True:
+    elif test_name == "module" and qinj == 'True':
         cmd = ["uv", "run", "python3", script_path, "--kcu", kcu_ip, "--test_chip", "--moduleid", moduleid, "--qinj", "--charges", charges]
         subprocess.run(cmd)
-    elif test_name == "module" and qinj == False:
+    elif test_name == "module" and qinj == 'False':
         cmd = ["uv", "run", "python3", script_path, "--kcu", kcu_ip, "--test_chip", "--moduleid", moduleid]
         subprocess.run(cmd)
 
@@ -23,7 +23,8 @@ def thread_target(test_name, kcu_ip, moduleid, qinj, charges):
         script_path = Path(__file__).parent.parent / "module_test_sw" / "test_module.py"
         run_uv_script(script_path, test_name, kcu_ip, moduleid, qinj, charges)
 
-def initiate_test(test_name, kcu_ip, moduleid, qinj=False, charges = [10,20,30]):
+
+def initiate_test(test_name, kcu_ip, moduleid, qinj='False', charges = '[10,20,30]'):
     t = threading.Thread(target=thread_target, args=(test_name, kcu_ip, moduleid, qinj, charges), daemon=True)
     t.start()
     t.join()

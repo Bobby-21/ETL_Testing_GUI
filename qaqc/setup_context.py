6d28e0a00fe49fe5de06dfdf47a8267d41c77add
Hayden Swanson <hayden_swanson22@yahoo.com>
2025-12-11 14:38:15 +0100

    more doc strings

--- qaqc/setup_context.py
+++ qaqc/setup_context.py
@@ -17,12 +17,22 @@ class SetupContext:
     
     @property
     def rb_config(self) -> Literal["modulev2", "rb7_modulev2", "rb6_modulev2"]:
+        """
+        Depending on the flavor choose one of these configs.
+        This config is used to instantiate a readout board.
+        """
         ...
 
     @property
     def module_ids(self) -> Dict[int, int]:
+        """
+        Tamalero requires a numerical number when instantiating.
+        Will probably just take the numerical part of the serial number.
+        """
         ...
 
-    @property
-    def db_test_base(self) -> Dict:
+    def db_test_base(self, slot: int) -> Dict:
+        """
+        A dictionary of all the information in SetupContext for the upload of a test of a module
+        """
         ...

f477dc9358f96e456f73224b03f9e795af6fdd7a
Hayden Swanson <hayden_swanson22@yahoo.com>
2025-12-16 13:17:10 +0100

    removed useless import

--- qaqc/tests/readout_board_communication.py
+++ qaqc/tests/readout_board_communication.py
@@ -2,7 +2,6 @@ from module_test_sw.tamalero.ReadoutBoard import ReadoutBoard
 from module_test_sw.tamalero.utils import get_kcu
 from qaqc import register, required
 from qaqc.errors import FailedTestCriteriaError
-from etlup import Tests
 from etlup.tamalero.ReadoutBoardCommunication import ReadoutBoardCommunicationV0
 
 @register(ReadoutBoardCommunicationV0)
@@ -30,18 +29,27 @@ def test(session):
         )
         rb = session.readout_board
 
-        # write to master lpgbt
-        val = rb.DAQ_LPGBT.rd_adr(0x1d7)
+        # read/write to master lpgbt
+        val = rb.DAQ_LPGBT.rd_reg("LPGBT.RWF.CHIPID.USERID0")
+        wr_temp = 0 if val !=0 else 1
+        rb.DAQ_LPGBT.wr_reg("LPGBT.RWF.CHIPID.USERID0", wr_temp)
+        wr_temp_check = rb.DAQ_LPGBT.rd_reg("LPGBT.RWF.CHIPID.USERID0")
+        if wr_temp != wr_temp_check:
+            raise FailedTestCriteriaError(f"Master LPGBT communication failed. Attempted write of {wr_temp} but read back {wr_temp_check}")
+        rb.DAQ_LPGBT.wr_reg("LPGBT.RWF.CHIPID.USERID0", val)
 
-        # Read to master lpgbt
+        # read/write from MUX64
+        mux_val = rb.MUX64.read_adc(63, calibrate=False) # this selects channel and reads it read + write
+        # assuming no error means success! checking if values make sense will be another test
 
-        # write from MUX64
-
-        # read from MUX64
-
-        # write to servant lpgbt
-
-        # Read to servant lpgbt
+        # read/write to servant lpgbt
+        val = rb.TRIG_LPGBT.rd_reg("LPGBT.RWF.CHIPID.USERID0")
+        wr_temp = 0 if val !=0 else 1
+        rb.TRIG_LPGBT.wr_reg("LPGBT.RWF.CHIPID.USERID0", wr_temp)
+        wr_temp_check = rb.TRIG_LPGBT.rd_reg("LPGBT.RWF.CHIPID.USERID0")
+        if wr_temp != wr_temp_check:
+            raise FailedTestCriteriaError(f"Servant LPGBT communication failed. Attempted write of {wr_temp} but read back {wr_temp_check}")
+        rb.TRIG_LPGBT.wr_reg("LPGBT.RWF.CHIPID.USERID0", val)
 
     except Exception as e:
         raise FailedTestCriteriaError(str(e))
